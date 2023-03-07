962ee0e514e2e28607bf5769f7bce7a3f7c1bdb1
Jan Lerking <jan.lerking@alfalaval.com>
2023-03-07 13:32:45 +0100

    0.0.1 Added base functions. /JL

--- tcx-package/tcx-package.go
+++ tcx-package/tcx-package.go
@@ -1,1 +1,17 @@
 package TCX_package
+
+const (
+	version = "0.0.1"
+)
+
+func Version() string {
+	return version
+}
+
+func TcxReadFile(filename string) {
+
+}
+
+func TcxWriteFile(filename string) {
+
+}
